f9e0af9727baa78bb75fbbfe40739f01f3eee065
Adam Wieckert <wiec1369@gmail.com>
2018-03-03 15:50:20 -0600

    Made Function that prints out searched country and their wins to the DOM.

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-
+// Object Challenge 1 ------!
 
 var players = [
     { 
@@ -33,8 +33,82 @@ for(var i = 0; i < players.length; i++){
 
 }
 
+if (winner === false){
+    var winnerDiv = document.getElementById('challenge-1');
+var printWinner = "<h1>The Winner is: NO ONE!!!!</h1>";
+printWinner += "<h2>With a Score of: FALIURE!!</h2>";
+winnerDiv.innerHTML = printWinner;
+} else {
+    var winnerDiv = document.getElementById('challenge-1');
+var printWinner = "<h1>The Winner is: " + winner + "</h1>";
+printWinner += "<h2>With a Score of: " + heighestScore + "!</h2>";
+winnerDiv.innerHTML = printWinner;
+}
+
 var winnerDiv = document.getElementById('challenge-1');
 var printWinner = "<h1>The Winner is: " + winner + "</h1>";
 printWinner += "<h2>With a Score of: " + heighestScore + "!</h2>";
 winnerDiv.innerHTML = printWinner;
+
+
+// Object Challenge 2---!
+
+var winnerList = [
+    {season: '1999-2000', team: 'Real Madrid', country: 'Spain'},
+    {season: '2000-2001', team: 'Bayern Munich', country: 'Germany'},
+    {season: '2001-2002', team: 'Real Madrid', country: 'Spain'},
+    {season: '2002-2003', team: 'Milan', country: 'Italy'},
+    {season: '2003-2004', team: 'Porto', country: 'Portugal'},
+    {season: '2004-2005', team: 'Liverpool', country: 'England'},
+    {season: '2005-2006', team: 'Barcelona', country: 'Spain'},
+    {season: '2006-2007', team: 'Milan', country: 'Italy'},
+    {season: '2007-2008', team: 'Manchester United', country: 'England'},
+    {season: '2008-2009', team: 'Barcelona', country: 'Spain'},
+    {season: '2009-2010', team: 'Internazionale', country: 'Italy'},
+    {season: '2010-2011', team: 'Barcelona', country: 'Spain'},
+    {season: '2011-2012', team: 'Chelsea', country: 'England'},
+    {season: '2012-2013', team: 'Bayern Munich', country: 'Germany'},
+    {season: '2013-2014', team: 'Real Madrid', country: 'Spain'},
+    {season: '2014-2015', team: 'Barcelona', country: 'Spain'},
+    {season: '2015-2016', team: 'Real Madrid', country: 'Spain'},
+    {season: '2016-2017', team: 'Real Madrid', country: 'Spain'},
+  ];
+
+
+  function champions (name) {
+
+
+    var numberOfWins = 0;
+
+    function stringBuilder (winner){
+        var buildElement = "<h1>You Searched for: " + winner + "</h1>";
+        buildElement += "<h3>They have: " + numberOfWins +" wins!</h3>";
+        return buildElement;
+      }
+    
+      function printToDom (stringToPrint, divID) {
+        var thingToPrint = document.getElementById(divID);
+        thingToPrint.innerHTML += stringToPrint;
+     }
+
+    for(var i = 0; i < winnerList.length; i++){
+        var currentWinner = winnerList[i].country;
+        if(currentWinner.toLocaleLowerCase() === name.toLocaleLowerCase()){
+            numberOfWins++;
+        }    
+    }
+    var builtString = stringBuilder(name);
+    printToDom(builtString, "challenge-2");
+
+}
+
+
+
+champions("spain");
+champions("england");
+
+
+// Object Challenge 2 End -----!
+
+
 
